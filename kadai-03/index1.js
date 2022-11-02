e7c87c2bc8b2d08cb801d37e03e15e8a2e5c56de
Ruiki-Harigai1103 <zliuweiji@gmail.com>
2022-11-02 04:30:13 +0000

    課題３の１を提出

---
+++ kadai-03/index1.js
@@ -0,0 +1,12 @@
+const number = parseInt(process.argv[2])
+let isPrime = true
+for(let yakusu = 2; yakusu < number; yakusu ++) {
+    if(number % yakusu ===0) {
+        is Prime = false
+        console.log(number, は , yakusu, 'で割り切れるので素数ではありません')
+        break
+    }
+}
+if(isPrime) {
+    console.log(number, 'は素数です')
+}
