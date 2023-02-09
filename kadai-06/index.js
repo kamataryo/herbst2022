48c35aec13eeb793287a8c7af9b7ae6b252bde15
Kamata, Ryo <mugil.cephalus+github@gmail.com>
2023-02-10 08:37:03 +0900

    Update index.js
    関数のコール方法、変数宣言のエラーを修正

--- kadai-06/index.js
+++ kadai-06/index.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const data = fs.readFileSync('./kadai-06/data.json').toString()
 let object
 try {
-    object = JSON.parse(json)
+    object = JSON.parse(data)
 } catch (error) {
     console.log('不正なJSONフォーマットです')
     process.exit(1)
 }
 
-object.count = object.count + 1 
-object.updateAt = date()
+object.count = object.count + 1
+object.updateAt = Date()
 
-fs.writeFileSync {(
+fs.writeFileSync(
     './kadai-06/data.json',
-    JSON.stringify(object)
+    JSON.stringify(object, null, 2)
     )
-console.log('object, null, 2')
-}
+
+console.log(object)
