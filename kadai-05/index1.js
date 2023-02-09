5a97c11e368185ebca86d751be0dfb6a8037370a
Kamata, Ryo <mugil.cephalus+github@gmail.com>
2023-02-10 08:08:24 +0900

    エラー修正
    "" (ダブルクオート)で括る事で文字列型になります。

--- kadai-05/index1.js
+++ kadai-05/index1.js
@@ -1,20 +1,20 @@
 const profile = {
-    name: 大豆,
+    name: "大豆",
     age: 20,
-    hobby: スポーツ観戦,
-    major: 地理学,
+    hobby: "スポーツ観戦",
+    major: "地理学",
 }
 const dictionary = {
-    name: 名前,
-    age: 年齢,
-    hobby: 趣味,
-    major: 専攻,
+    name: "名前",
+    age: "年齢",
+    hobby: "趣味",
+    major: "専攻",
 }
 
 const keys = Object.keys(profile)
 
 for (const key of keys) {
-    const subject = dictionary
-    const value = profile
-    console.log(私の + subject + は　+ value + です)
+    const subject = dictionary[key]
+    const value = profile[key]
+    console.log("私の" + subject + "は"　+ value + "です")
 }
